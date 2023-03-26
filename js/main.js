2db73a85f67c2888b6719872c9be776685c0c353
afranek <128502600+afranek@users.noreply.github.com>
2023-03-26 15:29:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function newF(n) {
+	if ((n > 100) & (n < 200)) {
+		console.log("Liczba znajduje się w przedziale");
+	} else {
+		console.log("Liczba nie znajduje sie w przedziale");
+	}
+}
+
+newF(101);
